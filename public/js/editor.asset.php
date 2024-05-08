8221a9573155c47eb5f8480ec831740e7fbad0ad
Jonathan Bossenger <jonathanbossenger@gmail.com>
2024-05-08 15:06:19 +0200

    Bug fixes

---
+++ public/js/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch'), 'version' => 'dec09f1e6b0f2aa53c4c');
---
+++ public/js/editor.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e={n:t=>{var o=t&&t.__esModule?()=>t.default:()=>t;return e.d(o,{a:o}),o},d:(t,o)=>{for(var r in o)e.o(o,r)&&!e.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:o[r]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t)};const t=window.wp.apiFetch;e.n(t)()({path:"/wp/v2/posts"}).then((e=>{console.log(e)}))})();
